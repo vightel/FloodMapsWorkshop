ffa496d702aae73a2a9d3d736729e8bd9abf947e
cappelaere <pat@cappelaere.com>
2014-07-11 11:53:37 -0400

    Updated modis processing

--- python/config.py
+++ python/config.py
@@ -6,6 +6,8 @@ HANDS_DIR			= DATA_DIR + "/HAND"
 HYSDROSHEDS_DIR		= DATA_DIR + "/HydroSHEDS"
 LANDSAT8_DIR		= DATA_DIR + "/landsat8"
 RADARSAT2_DIR		= DATA_DIR + "/radarsat2"
+MODIS_DIR			= DATA_DIR + "/modis"
+EO1_DIR				= DATA_DIR + "/eo1"
 
 version = "1.0 alpha3"
 
