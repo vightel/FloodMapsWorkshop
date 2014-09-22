3bd43549c40338eae4a21df6b7eb3b9a9d996fd5
cappelaere <pat@cappelaere.com>
2014-09-22 07:49:53 -0400

    Removed node_modules

--- node/node_modules/tokml/scripts/regenerate.js
+++
@@ -1,7 +0,0 @@
-var tokml = require('../'),
-    fs = require('fs'),
-    glob = require('glob');
-
-glob.sync('test/data/*.geojson').forEach(function(g) {
-    fs.writeFileSync(g.replace('.geojson', '.kml'), tokml(JSON.parse(fs.readFileSync(g))));
-});
