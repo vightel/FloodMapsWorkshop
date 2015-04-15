ea89b8f17a9edf521032fe94682a5f5058b0a7a1
Malaso <Malaso@users.noreply.github.com>
2015-04-15 14:35:01 -0500

    Update envs.copy.sh

--- envs.copy.sh
+++ envs.copy.sh
@@ -1,12 +1,12 @@
-export WORKSHOP_DIR=
+export WORKSHOP_DIR=/home/malaso/FloodMapsWorkshop
 export DBHOST=
 export DBNAME=
 export DBOWNER=
 export DBPORT=
 export PGPASS=
 export DATABASE_URL=
-export USGS_ACCOUNT=
-export USGS_PASSWORD=
+export USGS_ACCOUNT=malaso.susan@gmail.com
+export USGS_PASSWORD=Blessings2
 
 export FACEBOOK_APP_SECRET=
 export FACEBOOK_APP_ID=
@@ -18,8 +18,8 @@ export TWITTER_CREATOR_ID=
 export TWITTER_DOMAIN=
 export COOKIEHASH=
 
-export MAPBOX_PUBLIC_TOKEN=
-export MAPBOX_SECRET_TOKEN=
+export MAPBOX_PUBLIC_TOKEN=pk.eyJ1IjoibWFsYXNvIiwiYSI6IjVRRFU3ZVEifQ.tTVt2vO7ygn4PrxX80Ee-A
+export MAPBOX_SECRET_TOKEN=sk.eyJ1IjoibWFsYXNvIiwiYSI6ImFYRnlHN3MifQ.825rOt1fhl_5X1rEsqKdCg
 
 export AWS_ACCESSKEYID=
 export AWS_SECRETACCESSKEY=
